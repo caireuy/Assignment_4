98d91d163373c3f1ff074daa793d81149465f2fd
caireuy <croxas@academic.rrc.ca>
2024-10-20 22:55:10 -0500

    Added date and author name

--- pixell_transaction_report.py
+++ pixell_transaction_report.py
@@ -1,8 +1,8 @@
 """
 Description: A program that reads through transaction records and reports the results.
 Author: ACE Faculty
-Edited by: {Student Name}
-Date: {Date}
+Edited by: Carey Roxas
+Date: October 20, 2024
 Usage: This program will read transaction data from a .csv file, summarize and 
 report the results.
 """
